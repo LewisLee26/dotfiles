d13231470ec0348380a5844b7faafd5e79e448de
root <root@LAPTOP-3C0MHMSA.>
2024-09-22 19:07:33 +0100

    Added nvim <leader>p keymap

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -78,6 +78,8 @@ vim.opt.scrolloff = 10
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
