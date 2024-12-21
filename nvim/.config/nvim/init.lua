e52b2de8a299f1becaa957d2748abfb50e502a04
LewisLee26 <lewis.patrick.lee@gmail.com>
2024-12-21 14:05:05 +0000

    Seperated keymaps and added vscode support

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -74,47 +74,15 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
--- Remap C-a to increment numbers
-vim.api.nvim_set_keymap('n', '<C-a>', '<C-a>', { noremap = true, silent = true })
-
--- Remap C-x to decrement numbers
-vim.api.nvim_set_keymap('n', '<C-x>', '<C-x>', { noremap = true, silent = true })
-
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Paste without replacing buffer
-vim.keymap.set('x', '<leader>p', [["_dP]])
-
--- End of previous work
-vim.keymap.set({ 'n', 'v', 'x' }, '<S-e>', 'ge')
-
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<c-h>', '<c-w><c-h>', { desc = 'move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- Load different keymaps
+require 'custom.keymaps.default'
+if vim.g.vscode then
+  -- VSCode Neovim
+  require 'custom.keymaps.vscode'
+else
+  -- Ordinary Neovim
+  require 'custom.keymaps.terminal'
+end
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -129,15 +97,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
-
--- vim.keymap.set('i', '<C-c>', function()
---   vim.ui.input({ prompt = 'Calculator: ' }, function(input)
---     local calc = load('return ' .. (input or ''))()
---     if calc then
---       vim.api.nvim_feedkeys(tostring(calc), 'i', true)
---     end
---   end)
--- end)
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
---
+++ nvim/.config/nvim/lua/custom/keymaps/default.lua
@@ -0,0 +1,19 @@
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Remap C-z to increment numbers (C-a is used by wezterm)
+vim.api.nvim_set_keymap('n', '<C-z>', '<C-a>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-z>', '<C-o><C-a>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-z>', '<C-a>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('c', '<C-z>', '<C-a>', { noremap = true, silent = true })
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Paste without replacing buffer
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
+-- End of previous work
+vim.keymap.set({ 'n', 'v', 'x' }, '<S-e>', 'ge')
