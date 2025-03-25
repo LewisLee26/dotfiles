ad66e91c6a922972034c442564d35d98abeeba26
LewisLee26 <lewis.patrick.lee@gmail.com>
2025-03-25 10:22:09 +0000

    Added abbrs and zoxide init

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -22,3 +22,20 @@ if status is-interactive
     set fish_cursor_visual block
     fish_vi_key_bindings
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/anaconda3/bin/conda
+    eval /opt/homebrew/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/homebrew/anaconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/homebrew/anaconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
+abbr -a -- cat bat
+abbr -a -- ls eza
+zoxide init fish | source
