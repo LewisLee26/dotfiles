97cef7ae0334d2547f42d285aa507fc2a8d626f3
LewisLee26 <lewis.patrick.lee@gmail.com>
2025-04-14 11:20:21 +0100

    Added search history forward and backward key binds for fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -36,6 +36,10 @@ abbr -a -- pullr git pull origin main --rebase
 
 bind -M default \cF accept-autosuggestion
 bind -M insert -m default \cF accept-autosuggestion -m insert 
+bind -M default \cP history-search-backward
+bind -M default \cN history-search-forward
+bind -M insert -m default \cP history-search-backward -m insert 
+bind -M insert -m default \cN history-search-forward -m insert
 
 zoxide init fish | source
 starship init fish | source
